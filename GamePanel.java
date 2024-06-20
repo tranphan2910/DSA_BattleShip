a538d33d1f7a7e1f1102f11290d77ce9fbe59af3
Nhanguyen13 <145524985+Nhanguyen13@users.noreply.github.com>
2024-06-20 21:36:28 +0700

    Game Panel

---
+++ GamePanel.java
@@ -0,0 +1,213 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+
+public class GamePanel extends JPanel implements MouseListener, MouseMotionListener {
+
+    public enum GameState { PlacingShips, FiringShots, GameOver }
+
+
+    private StatusPanel statusPanel;
+    private SelectionGrid computer;
+    private SelectionGrid player;
+    private BattleshipAI aiController;
+    private Ship placingShip;
+    private Position tempPlacingPosition;
+    private int placingShipIndex;
+    private GameState gameState;
+    public static boolean debugModeActive;
+    public GamePanel(int aiChoice) {
+        computer = new SelectionGrid(0,0);
+        player = new SelectionGrid(0,computer.getHeight()+50);
+        // setBackground(new Color(42, 136, 163));
+        setBackground(new Color(194,194,194));
+        setPreferredSize(new Dimension(computer.getWidth(), player.getPosition().y + player.getHeight()));
+        addMouseListener(this);
+        addMouseMotionListener(this);
+        if(aiChoice == 0) aiController = new SimpleRandomAI(player);
+        else aiController = new SmarterAI(player,aiChoice == 2,aiChoice == 2);
+        statusPanel = new StatusPanel(new Position(0,computer.getHeight()+1),computer.getWidth(),49);
+        restart();
+    }
+
+
+    public void paint(Graphics g) {
+        super.paint(g);
+        computer.paint(g);
+        player.paint(g);
+        if(gameState == GameState.PlacingShips) {
+            placingShip.paint(g);
+        }
+        statusPanel.paint(g);
+    }
+
+
+    public void handleInput(int keyCode) {
+        if(keyCode == KeyEvent.VK_ESCAPE) {
+            System.exit(1);
+        } else if(keyCode == KeyEvent.VK_R) {
+            restart();
+        } else if(gameState == GameState.PlacingShips && keyCode == KeyEvent.VK_Z) {
+            placingShip.toggleSideways();
+            updateShipPlacement(tempPlacingPosition);
+        } else if(keyCode == KeyEvent.VK_D) {
+            debugModeActive = !debugModeActive;
+        }
+        repaint();
+    }
+
+
+    public void restart() {
+        computer.reset();
+        player.reset();
+        player.setShowShips(true);
+        aiController.reset();
+        tempPlacingPosition = new Position(0,0);
+        placingShip = new Ship(new Position(0,0),
+                new Position(player.getPosition().x,player.getPosition().y),
+                SelectionGrid.BOAT_SIZES[0], true);
+        placingShipIndex = 0;
+        updateShipPlacement(tempPlacingPosition);
+        computer.populateShips();
+        debugModeActive = false;
+        statusPanel.reset();
+        gameState = GameState.PlacingShips;
+    }
+
+
+    private void tryPlaceShip(Position mousePosition) {
+        Position targetPosition = player.getPositionInGrid(mousePosition.x, mousePosition.y);
+        updateShipPlacement(targetPosition);
+        if(player.canPlaceShipAt(targetPosition.x, targetPosition.y,
+                SelectionGrid.BOAT_SIZES[placingShipIndex],placingShip.isSideways())) {
+            placeShip(targetPosition);
+        }
+    }
+
+
+    private void placeShip(Position targetPosition) {
+        placingShip.setShipPlacementColour(Ship.ShipPlacementColour.Placed);
+        player.placeShip(placingShip,tempPlacingPosition.x,tempPlacingPosition.y);
+        placingShipIndex++;
+        if(placingShipIndex < SelectionGrid.BOAT_SIZES.length) {
+            placingShip = new Ship(new Position(targetPosition.x, targetPosition.y),
+                    new Position(player.getPosition().x + targetPosition.x * SelectionGrid.CELL_SIZE,
+                            player.getPosition().y + targetPosition.y * SelectionGrid.CELL_SIZE),
+                    SelectionGrid.BOAT_SIZES[placingShipIndex], true);
+            updateShipPlacement(tempPlacingPosition);
+        } else {
+            gameState = GameState.FiringShots;
+            statusPanel.setTopLine("Attack the Computer!");
+            statusPanel.setBottomLine("Destroy all Ships to win!");
+        }
+    }
+
+
+    private void tryFireAtComputer(Position mousePosition) {
+        Position targetPosition = computer.getPositionInGrid(mousePosition.x,mousePosition.y);
+        if(!computer.isPositionMarked(targetPosition)) {
+            doPlayerTurn(targetPosition);
+            // Only do the AI turn if the game didn't end from the player's turn.
+            if(!computer.areAllShipsDestroyed()) {
+                doAITurn();
+            }
+        }
+    }
+
+
+    private void doPlayerTurn(Position targetPosition) {
+        boolean hit = computer.markPosition(targetPosition);
+        String hitMiss = hit ? "Hit" : "Missed";
+        String destroyed = "";
+        if(hit && computer.getMarkerAtPosition(targetPosition).getAssociatedShip().isDestroyed()) {
+            destroyed = "(Destroyed)";
+        }
+        statusPanel.setTopLine("Player " + hitMiss + " " + targetPosition + destroyed);
+        if(computer.areAllShipsDestroyed()) {
+            gameState = GameState.GameOver;
+            statusPanel.showGameOver(true);
+        }
+    }
+
+
+    private void doAITurn() {
+        Position aiMove = aiController.selectMove();
+        boolean hit = player.markPosition(aiMove);
+        String hitMiss = hit ? "Hit" : "Missed";
+        String destroyed = "";
+        if(hit && player.getMarkerAtPosition(aiMove).getAssociatedShip().isDestroyed()) {
+            destroyed = "(Destroyed)";
+        }
+        statusPanel.setBottomLine("Computer " + hitMiss + " " + aiMove + destroyed);
+        if(player.areAllShipsDestroyed()) {
+            gameState = GameState.GameOver;
+            statusPanel.showGameOver(false);
+        }
+    }
+
+    private void tryMovePlacingShip(Position mousePosition) {
+        if(player.isPositionInside(mousePosition)) {
+            Position targetPos = player.getPositionInGrid(mousePosition.x, mousePosition.y);
+            updateShipPlacement(targetPos);
+        }
+    }
+
+
+    private void updateShipPlacement(Position targetPos) {
+        if(placingShip.isSideways()) {
+            targetPos.x = Math.min(targetPos.x, SelectionGrid.GRID_WIDTH - SelectionGrid.BOAT_SIZES[placingShipIndex]);
+        } else {
+            targetPos.y = Math.min(targetPos.y, SelectionGrid.GRID_HEIGHT - SelectionGrid.BOAT_SIZES[placingShipIndex]);
+        }
+        placingShip.setDrawPosition(new Position(targetPos),
+                new Position(player.getPosition().x + targetPos.x * SelectionGrid.CELL_SIZE,
+                        player.getPosition().y + targetPos.y * SelectionGrid.CELL_SIZE));
+        tempPlacingPosition = targetPos;
+        if(player.canPlaceShipAt(tempPlacingPosition.x, tempPlacingPosition.y,
+                SelectionGrid.BOAT_SIZES[placingShipIndex],placingShip.isSideways())) {
+            placingShip.setShipPlacementColour(Ship.ShipPlacementColour.Valid);
+        } else {
+            placingShip.setShipPlacementColour(Ship.ShipPlacementColour.Invalid);
+        }
+    }
+
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        Position mousePosition = new Position(e.getX(), e.getY());
+        if(gameState == GameState.PlacingShips && player.isPositionInside(mousePosition)) {
+            tryPlaceShip(mousePosition);
+        } else if(gameState == GameState.FiringShots && computer.isPositionInside(mousePosition)) {
+            tryFireAtComputer(mousePosition);
+        }
+        repaint();
+    }
+
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        if(gameState != GameState.PlacingShips) return;
+        tryMovePlacingShip(new Position(e.getX(), e.getY()));
+        repaint();
+    }
+
+
+    @Override
+    public void mouseClicked(MouseEvent e) {}
+
+    @Override
+    public void mousePressed(MouseEvent e) {}
+
+    @Override
+    public void mouseEntered(MouseEvent e) {}
+
+    @Override
+    public void mouseExited(MouseEvent e) {}
+
+    @Override
+    public void mouseDragged(MouseEvent e) {}
+}
