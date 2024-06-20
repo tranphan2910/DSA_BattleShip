f1d9b5beb553f7b82a296a8a82c777b08cff7623
Nhanguyen13 <145524985+Nhanguyen13@users.noreply.github.com>
2024-06-20 21:35:57 +0700

    Game

---
+++ Game.java
@@ -0,0 +1,44 @@
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Game implements KeyListener {
+    public static void main(String[] args) {
+        Game game = new Game();
+    }
+
+    private GamePanel gamePanel;
+
+    public Game() {
+
+        String[] options = new String[] {"Easy", "Medium", "Hard"};
+        String message = "Easy will make moves entirely randomly,\nMedium will focus on areas where it finds ships,"
+                + "\nand Hard will make smarter choices over Medium.";
+        int difficultyChoice = JOptionPane.showOptionDialog(null, message,
+                "Choose an AI Difficulty",
+                JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE,
+                null, options, options[0]);
+
+        JFrame frame = new JFrame("Battleship");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+
+        gamePanel = new GamePanel(difficultyChoice);
+        frame.getContentPane().add(gamePanel);
+
+        frame.addKeyListener(this);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        gamePanel.handleInput(e.getKeyCode());
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {}
+
+    @Override
+    public void keyReleased(KeyEvent e) {}
+}
