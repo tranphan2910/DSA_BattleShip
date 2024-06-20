a3bbc746b52e267454cc8c2fd1ab8c9363411275
la la <tranphan2910@gmail.com>
2024-06-20 21:42:47 +0700

    add Rechtangle class

---
+++ battleship/src/Rectangle.java
@@ -0,0 +1,30 @@
+
+public class Rectangle {
+    protected Position position;
+    protected int width;
+    protected int height;
+/* The first constructor of the Rectangle */
+    public Rectangle(Position position, int width, int height) {
+        this.position = position;
+        this.width = width;
+        this.height = height;
+    }
+    public Rectangle(int x, int y, int width, int height) {
+        this(new Position(x,y), width, height);
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+    public boolean isPositionInside(Position targetPosition) {
+        return targetPosition.x >= position.x && targetPosition.y >= position.y && targetPosition.x < position.x + width && targetPosition.y < position.y + height;
+    }
+}
