24ee7634a26b5873f42a71830af54c8919497096
Nhanguyen13 <145524985+Nhanguyen13@users.noreply.github.com>
2024-06-20 21:31:15 +0700

    Battleship AI

---
+++ BattleshipAI.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BattleshipAI {
+    protected List<Position> validMoves;
+    protected SelectionGrid playerGrid;
+
+    public BattleshipAI(SelectionGrid playerGrid) {
+        this.playerGrid = playerGrid;
+        createValidMoveList();
+    }
+
+    public Position selectMove() {
+        return Position.ZERO;
+    }
+
+    public void reset() {
+        createValidMoveList();
+    }
+
+    private void createValidMoveList() {
+        validMoves = new ArrayList<>();
+        for(int x = 0; x < SelectionGrid.GRID_WIDTH; x++) {
+            for(int y = 0; y < SelectionGrid.GRID_HEIGHT; y++) {
+                validMoves.add(new Position(x,y));
+            }
+        }
+    }
+}
