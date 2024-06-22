012a0cca3784bfeb318eb76d792101a2fb6676c3
Tran Phan <124681260+tranphan2910@users.noreply.github.com>
2024-06-22 22:31:51 +0700

    Update SimpleRandomAI.java

--- battleship/src/SimpleRandomAI.java
+++ battleship/src/SimpleRandomAI.java
@@ -1,3 +1,7 @@
+/* Name: Phan Bảo Trân
+ ID: ITDSIU21125
+ Purpose: The SimpleRandomAI class implements a simple random selection strategy for making moves in a Battleship game. By shuffling the list of valid moves both during initialization and reset, the AI ensures that its move selections are random. The selectMove() method retrieves the next move from the list, providing a straightforward and effective way to play the game without repeating moves. This random selection strategy makes the AI unpredictable and can be a useful baseline for more complex AI strategies.
+*/
 import java.util.Collections;
 
 public class SimpleRandomAI extends BattleshipAI{
